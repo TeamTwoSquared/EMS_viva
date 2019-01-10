2a42fede00acdf62a1e8522909b618b4a279de7c
sajun sandaruwan <sajunsandaruwan@gmail.com>
2019-01-10 05:43:40 +0530

    Update supportConditions.blade.php

--- resources/views/admin/client/supportConditions.blade.php
+++ resources/views/admin/client/supportConditions.blade.php
@@ -2,25 +2,23 @@
 @section('content')
 
 
-<section class="au-breadcrumb2" data-pg-collapsed> 
+<section class="au-breadcrumb2 pad-bottom5 pad15" data-pg-collapsed> 
     <div class="container"> 
         <div class="row"> 
             <div class="col-md-12"> 
                 <div class="au-breadcrumb-content"> 
-                    <div class="au-breadcrumb-left"> 
+                    <div class="au-breadcrumb-left" data-pg-collapsed> 
                         <span class="au-breadcrumb-span">You are here:</span> 
                         <ul class="list-unstyled list-inline au-breadcrumb__list"> 
                             <li class="list-inline-item active"> 
-                                <a href="#">Home</a> 
-                            </li>                             
-                            <li class="list-inline-item seprate"> 
-                                <span>/</span> 
-                            </li>                             
-                            <li class="list-inline-item">Support Center</li>                             
-                        </ul>                         
+                                <a href="dash">Home</a> 
+                            </li>         
+                            
+                        </ul>     
                     </div>                     
-                    <form class="au-form-icon--sm" action="" method="post"> 
-                        <input class="au-input--w300 au-input--style2" type="text" placeholder="Search for datas &amp; reports..."> 
+                    <form class="au-form-icon--sm" action="/client/search" method="post">
+                        {{ csrf_field() }} 
+                        <input class="au-input--w300 au-input--style2" name = "data" type="text" placeholder="Find Services...."> 
                         <button class="au-btn--submit2" type="submit"> 
                             <i class="zmdi zmdi-search"></i> 
                         </button>                         
@@ -47,9 +45,7 @@ And we will make sure to protect your personel information and not to misuse the
                                  
                         </div>
                 </div>          
-            </div>   
-            
-            
+            </div>     
             @include('inc.rightAds')             
         </div>         
         <hr/> 
