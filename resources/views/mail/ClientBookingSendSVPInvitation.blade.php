2811fbbd77293bc23608f17c7ce991db2ae5f9a6
sajun sandaruwan <sajunsandaruwan@gmail.com>
2019-01-11 05:42:36 +0530

    Update ClientBookingSendSVPInvitation.blade.php

--- resources/views/mail/ClientBookingSendSVPInvitation.blade.php
+++ resources/views/mail/ClientBookingSendSVPInvitation.blade.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\BookingsController;
 
 $booking = BookingsController::getBooking($data['booking_id']);
 $client = ClientsController::getClient();
-$service=servicesController::getService($data['service']);
+$service=ServicesController::getService($data['service']);
 @endphp
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
