6cfc6d5d91baabe1afff67147d7cd76f578246e2
sajun sandaruwan <sajunsandaruwan@gmail.com>
2019-01-11 08:30:53 +0530

    Update ServicesController.php

--- app/Http/Controllers/service/ServicesController.php
+++ app/Http/Controllers/service/ServicesController.php
@@ -117,7 +117,7 @@ class ServicesController extends Controller
           //  DB::table('services')->update(['service_id'=>($request->serviceID),'name'=>$request->sName , 'price'=>$request->price,'description'=>$request->description ]);
 
 		  $validator = Validator::make($request->all(), [
-            'name' => 'required|unique:services|regex:/[a-zA-Z]+$/u',
+            'name' => 'required|regex:/[a-zA-Z]+$/u',
             'description' => 'required|max:100|regex:/[a-zA-Z]+$/u',
         ]);
 
